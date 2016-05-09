d7f99930850ee7de688bf224308fb368569aa020
cvb <peter@standalone.su>
2016-05-09 23:16:34 +0300

    Add some core fetch and store utilities

--- src/com_mon/core.clj
+++ src/com_mon/core.clj
@@ -1,4 +1,77 @@
 (ns com-mon.core
-  (:gen-class))
+  (:gen-class)
+  (:require [environ.core :refer [env]]
+            [clojure.core.match :as m]
+            [clojure.zip :as z]
+            [clojurewerkz.neocons.rest :as nr]
+            [clj-time.core :as tt]
+            [clj-time.local :as tl]
+            [clj-time.format :as tf]
 
-(defn -main [])
+            [com-mon.vk :as vk]
+            [com-mon.fetcher :as f]
+            [com-mon.db.neo4j :as neo]
+            [amazonica.aws.s3 :as s3]))
+
+
+(def vk-lentach-last-comments
+  [{:group (fn [_] (vk/groups-getbyid "oldlentach"))
+    :children
+    [{:post (fn [loc] (vk/wall-get (vk/group-id loc) {:count 100}))
+      :children
+      [{:comment (fn [loc] (vk/wall-getcomments
+                             (vk/group-id loc)
+                             (-> (vk/parent loc :post) :id)
+                             {:count 100}))
+        :children
+        [{:like (fn [loc] (vk/likes-getlist
+                            (vk/group-id loc)
+                            (-> (vk/parent loc :comment) :id)
+                            {:count 100}))}]}]}]}])
+
+(defn process-vk-fetched
+  [data &{:keys [save-group save-post save-comment save-like]}]
+  (loop [loc (f/zipper data)]
+    (if (z/end? loc)
+      (z/root loc)
+      (if (sequential? (z/node loc))
+        (recur (z/next loc))
+        (do
+          (m/match (vk/node-type (z/node loc))
+            :group   (save-group   (z/node loc))
+            :post    (save-post    (z/node loc) (-> loc z/up z/node))
+            :comment (save-comment (z/node loc) (-> loc z/up z/node))
+            :like    (save-like    (z/node loc) (-> loc z/up z/node)))
+          (recur (z/next loc)))))))
+
+(defn update-neo4j-vk [data]
+  (let [conn (neo/conn)]
+    (process-vk-fetched data
+      :save-group   (partial neo/save-group conn)
+      :save-post    (partial neo/save-post conn)
+      :save-comment (partial neo/save-comment conn)
+      :save-like    (partial neo/save-like conn))))
+
+(defn fetch-last-vk []
+  (let [[v fails] (f/run-sync vk-lentach-last-comments)]
+    (if-not (empty? fails)
+      (throw (ex-info "Some requests are failed" {:causes fails}))
+      v)))
+
+(defn get-vk-l-s3 []
+  (let [lastf (neo/get-vk-l-last (neo/conn))
+        all (->> (s3/list-objects :bucket-name :s-stuff :prefix "vk-l/updates")
+              :object-summaries
+              (map :key)
+              sort
+              reverse
+              (take-while #(not= lastf %))
+              reverse)]
+    (doseq [[n e] (map vector (range) all)]
+      (println (+ 1 n) "of" (count all) e)
+      (-> (s3/get-object :bucket-name :s-stuff :key e)
+        :input-stream
+        slurp
+        read-string
+        update-neo4j-vk)
+      (neo/set-vk-l-last (neo/conn) e))))
---
+++ src/com_mon/db/neo4j.clj
@@ -0,0 +1,63 @@
+(ns com-mon.db.neo4j
+  (:require [clojurewerkz.neocons.rest :as nr]
+            [clojurewerkz.neocons.rest.cypher :as cy]
+            [environ.core :refer [env]]))
+
+(defn conn []
+  (nr/connect (env :neo4j-url)))
+
+(defn save-group
+  [conn node]
+  (cy/tquery conn
+    "merge (a:group:user{id: {g}.id})
+     on create set a={g}"
+    {:g (update (select-keys (:group node) [:id :name :screen_name])
+          :id -)}))
+
+(defn save-post
+  [conn post group]
+  (cy/tquery conn
+    "match (g:group{id: {gid}.id})
+     merge (p:post{id: {post}.id})
+     on create set p={post}
+     merge (a:user{id: {uid}.id})
+     on create set a={uid}
+     merge (g)-[:have]->(p)
+     merge (a)-[:make]->(p)"
+    {:gid {:id (-> group :group :id -)}
+     :uid {:id (:from_id (:post post))}
+     :post (select-keys (:post post) [:owner_id :date :from_id :id :text])}))
+
+(defn save-comment
+  [conn comment post]
+  (cy/tquery conn
+    "match (p:post{id: {pid}.id})
+     merge (c:comment{id: {cmt}.id})
+     on create set c={cmt}
+     merge (a:user{id: {usr}.id})
+     on create set a={usr}
+     merge (p)-[:have]->(c)
+     merge (a)-[:make]->(c)"
+    {:pid {:id (:id (:post post))}
+     :usr {:id (:from_id (:comment comment))}
+     :cmt (select-keys (:comment comment)
+            [:owner_id :date :from_id :id :text])}))
+
+(defn save-like
+  [conn like comment]
+  (cy/tquery conn
+    "match (c:comment{id: {cid}})
+     merge (u:user{id: {uid}.id})
+     on create set u={uid}
+     merge (u)-[:like]->(c)"
+    {:cid (:id (:comment comment)) :uid {:id (:like like)}}))
+
+(defn get-vk-l-last [conn]
+  (some-> (cy/tquery conn "match (n:updates) return n.file as file")
+    first
+    (get "file")))
+
+(defn set-vk-l-last [conn fname]
+  (cy/tquery conn
+    "merge (n:updates) on create set n = {f} on match set n = {f}"
+    {:f {:file fname}}))
